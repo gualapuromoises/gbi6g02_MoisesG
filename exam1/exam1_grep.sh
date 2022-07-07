b45e7ff0ee68cc1a0312922565f887f079f085c9
Jairo Calapucha <jairo.calapucha@ikiam.edu.ec>
2022-07-07 11:54:44 -0500

    Se esta creando un script

--- exam1/exam1_grep.sh
+++ exam1/exam1_grep.sh
@@ -1,2 +1,6 @@
-# En este archivo se generará un script con nombre de exam1_grep.sh
+#####En este archivo se generará un script con nombre de exam1_grep.sh
+#! /bin/bash
+
+cat ../data/data_INFg.txt
+mkdir grep 
 
